7cae8a5e74976c6c4a88452606a1eb053ef0f9c8
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2025-04-04 17:24:54 -0700

    refactor: update import statement for main function in __main__.py
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- ufo/__main__.py
+++ ufo/__main__.py
@@ -1,7 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-from ufo import ufo
+from ufo.ufo import main as main
+
 
 if __name__ == "__main__":
     # Execute the main script
-    ufo.main()
+    main()
